b6c21d18d5f29cb1fb83d5b62b84e7cd7ef5e657
Chirag Bangera <cbepic2@gmail.com>
2024-02-12 17:38:13 -0500

    Add clipboard copy and paste commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,18 +10,40 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codeguardian" is now active!');
+	console.log('Congratulations, your extension "CodeGuardian" is now active!');
+	
+	const logFilePath = path.join(context.extensionPath, 'copy-paste-log.txt');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codeguardian.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeGuardian!');
-	});
+    function logToFile(message: string) {
+        const timestamp = new Date().toISOString();
+        fs.appendFileSync(logFilePath, `${timestamp}: ${message}\n`, { encoding: 'utf8' });
+    }
 
-	context.subscriptions.push(disposable);
+    let lastSelection = new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(0, 0));
+    let likelyCopiedText = '';
+
+    vscode.window.onDidChangeTextEditorSelection(event => {
+        if (!event.selections[0].isEqual(lastSelection)) {
+            lastSelection = event.selections[0];
+            if (!event.selections[0].isEmpty) {
+                likelyCopiedText = event.textEditor.document.getText(event.selections[0]);
+            }
+        }
+    }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (event.contentChanges.length > 0) {
+            let change = event.contentChanges[0];
+            if (change.text.length > 1 || change.text === '\n') { // Simple heuristic to filter out single character changes
+                if (change.text === likelyCopiedText) {
+                    logToFile('Copied then pasted content: ' + change.text.substring(0, 100)); // Log only the first 100 chars for brevity
+                } else {
+                    logToFile('Pasted content from external source');
+                }
+            }
+        }
+    }, null, context.subscriptions);
+	
 }
 
 // This method is called when your extension is deactivated
